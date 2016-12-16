45986806303549ea7dcc509e10ef16538af7f18d
wangyy <wangyy@Ctrip.com>
2016-12-16 14:07:56 +0800

    第一次修改

---
+++ forex-service/src/main/java/com/ctrip/forex/order/dao/orderdao.java
@@ -0,0 +1,7 @@
+package com.ctrip.forex.order.dao.dao;
+
+/**
+ * Created by wangyy on 2016/12/13.
+ */
+public class orderdao {
+}
---
+++ forex-service/src/main/java/com/ctrip/forex/order/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.ctrip.forex.order.dao.service;
+
+/**
+ * Created by wangyy on 2016/12/13.
+ */
+public class OrderService {
+}
